59e89a38cbc65577e47c6af567fceb642101196b
LedinaShahi <ledina.shahi@fshnstudent.info>
2022-05-06 13:43:14 +0200

    1

--- src/main/java/com/internship/HRapp/entity/Users.java
+++ src/main/java/com/internship/HRapp/entity/Users.java
@@ -5,8 +5,6 @@ import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.UUID;
 
 @Entity

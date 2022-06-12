823db19af8c60b0a744112214c335a6ffc55707a
MeridianMucmara <ridimucmara@gmail.com>
2022-06-12 21:56:20 +0200

    versioni 1.2

---
+++ src/main/java/com/internship/HRapp/security/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.internship.HRapp.security;
+
+import com.internship.HRapp.configuration.CustomAuthenticationFilter;
+import com.internship.HRapp.service.concretes.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserServiceImpl userService;
+    private final CustomAuthenticationFilter authenticationFilter;
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(userService);
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable()
+                    .authorizeRequests()
+                    .antMatchers(AUTH_WHITELIST)
+                    .permitAll().anyRequest()
+                    .authenticated().and()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        }
+
+        @Override
+        @Bean
+        public AuthenticationManager authenticationManagerBean() throws Exception {
+            return super.authenticationManagerBean();
+        }
+
+
+        @Override
+        public void configure(WebSecurity web) {
+            web.ignoring().antMatchers(AUTH_WHITELIST);
+        }
+
+        private static final String[] AUTH_WHITELIST = {
+                "/v3/api-docs/**",
+                "/swagger-ui/**",
+                "/api/v1/hr_management_system/users/getAll/**",
+                "/hr_management_system/login/**",
+                "/api/v1/hr_management_system/users/login/**",
+                "/api/auth/authenticate/**",
+
+
+};
+    }
+
